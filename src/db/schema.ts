8f2097792ddb45173cf6ef7642482958126883f0
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-02-04 18:35:30 +0100

    spoljni kljucevi; asocijativne klase

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -13,6 +13,17 @@ export const campaignsTable = pgTable("Campaign", {
   name: varchar({ length: 255 }).notNull(),
   description: varchar({ length: 255 }).notNull(),
   dateStart: timestamp().defaultNow(),
+  gameMaster: uuid().notNull().references(() => usersTable.id),
+});
+
+export const campaignPlayersTable =pgTable("campainPlayers", {
+  capmaign: uuid().notNull().references(() => campaignsTable.id),
+  player: uuid().notNull().references(() => usersTable.id),
+});
+
+export const campaignDocumentsTable =pgTable("campaignDocuments", {
+  capmaign: uuid().notNull().references(() => campaignsTable.id),
+  document: uuid().notNull().references(() => documentsTable.id),
 });
 
 export const documentsTable = pgTable("Document", {
@@ -29,9 +40,11 @@ export const charSheetsTable = pgTable("CharSheet", {
   armor: integer().notNull(),
   hp: integer().notNull(),
   currency: numeric().notNull(),
+  owner: uuid().notNull().references(() => usersTable.id),
 })
 
 export const notesTable = pgTable("Note", {
   id: uuid().primaryKey().defaultRandom(),
-  content: varchar({ length: 255 })
+  content: varchar({ length: 255 }).notNull(),
+  writtenIn: uuid().notNull().references(() => charSheetsTable.id),
 })
