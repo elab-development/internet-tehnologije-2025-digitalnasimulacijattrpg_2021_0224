f4e0ff8e897bcc5fc3e2910850edd636f6c79248
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-02-06 20:12:22 +0100

    objekti

---
+++ src/app/types.ts
@@ -0,0 +1,45 @@
+import { UUID } from "crypto";
+
+export enum fileType {
+    pdf,
+    png,
+    jpeg,
+}
+
+export type user = {
+    id : UUID,
+    username: string,
+}
+
+export type campaign = {
+    id : UUID,
+    name : string,
+    description : string,
+    dateStart : Date,
+    gameMaster : user | null, // privremeno nullable
+}
+
+export type document = {
+    id : UUID,
+    name : string,
+    type : fileType,
+    filepath : string,
+}
+
+export type charSheet = {
+    id : UUID,
+    name : string,
+    str : number,
+    dex : number,
+    will : number,
+    armor : number,
+    hp : number,
+    currency : number,
+    owner : user | null, // privremeno nullable
+}
+
+export type note = {
+    id : UUID,
+    content : string,
+    writtenIn : charSheet,
+}
