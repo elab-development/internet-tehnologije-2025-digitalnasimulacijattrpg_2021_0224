f203bdc62d5bb19c5291cc82f0fd3ca74f3dfd84
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-02-07 00:54:17 +0100

    neka estetika; ne znam kako se to promenilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Kockice from "./components/kockice";
 
 export default function Welcome() {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center border">
             <h1>Stranica koju vide neulogovani korisnici</h1>
             <p>*lorem ipsum*</p>
             <Kockice />
