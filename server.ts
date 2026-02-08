991453f90aa2f76ef7f89ea214bf1b75be02c952
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-02-08 17:11:17 +0100

    sesija prototip 1
    -uspesan test komunikacije klijenta i servera
    -uspesno smestanje klijenta u zeljenu "sobu" koja ce se koristiti kao
    sesija kampanje

--- server.ts
+++ server.ts
@@ -9,13 +9,29 @@ const port = 3000
 const app = next({dev, hostname, port})
 const handler = app.getRequestHandler()
 
+let sessions : Array<string> = []
+
 app.prepare().then(() => {
     const httpServer = createServer(handler)
 
     const io = new Server(httpServer)
 
     io.on("connection", (socket) => {
-        // server kod
+        console.log(socket.id + " logged in")
+        socket.emit("pisi", "dobrodosao")
+        socket.on("joinCampaign", (campaign) => {
+            socket.rooms.forEach(room => socket.leave(room))
+            if (!sessions.includes(campaign)) {
+                sessions.push(campaign)
+                console.log("> New session started.")
+                console.log("> Active sessions: " + sessions)
+            }
+            socket.emit("pisi", "test")
+            socket.join(campaign)
+            //console.log(io.sockets.adapter.rooms)
+            console.log("> Player " + socket.id + " joined " + campaign)
+            io.to(campaign).emit("pisi", "dobrodosao u " + campaign)
+        })
     })
 
     httpServer
