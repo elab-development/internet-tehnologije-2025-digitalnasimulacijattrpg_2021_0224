2df8eaca1632b572cdf516332ee556c61c7e02e7
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-02-14 14:21:24 +0100

    ugasio spam u konzoli

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,26 +7,26 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     const token = (await cookies()).get(AUTH_COOKIE)?.value;
-    console.log("Token retrieved:", token);
+    //console.log("Token retrieved:", token);
 
     if (!token) {
-        console.log("No token found, returning unauthenticated response.");
+        //console.log("No token found, returning unauthenticated response.");
         return NextResponse.json({ user: null });
     }
 
     try {
         const claims = vertifyAuthToken(token);
-        console.log("Claims from token:", claims);
+        //console.log("Claims from token:", claims);
 
         const [user] = await db
             .select({ id: usersTable.id, username: usersTable.username })
             .from(usersTable)
             .where(eq(usersTable.id, claims.sub));
         
-        console.log("User found:", user);
+        //console.log("User found:", user);
         return NextResponse.json({ user: user ?? null });
     } catch (error) {
-        console.error("Error verifying token or querying database:", error);
+        //console.error("Error verifying token or querying database:", error);
         return NextResponse.json({ user: null }, { status: 401 });
     }
 }
