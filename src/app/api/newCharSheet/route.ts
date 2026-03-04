4ba1d41554e9bd7bde5ec4be6bac43b9a99b5d8e
Lucidonius <55060817+VectorOfAcceleration@users.noreply.github.com>
2026-03-04 17:51:36 +0100

    [DEV] CORS zastita (Logovanje van dozvoljenih ruta)

--- src/app/api/newCharSheet/route.ts
+++ src/app/api/newCharSheet/route.ts
@@ -2,6 +2,18 @@ import { db } from '../../../db';
 import { charSheetsTable } from '../../../db/schema';
 import { NextResponse } from 'next/server';
 
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  });
+}
+
 export async function POST(req:Request) {
     try{
     if(req.method!=="POST"){
--- src/app/api/youtube/route.ts
+++ src/app/api/youtube/route.ts
@@ -1,5 +1,17 @@
 import { NextResponse,NextRequest } from "next/server";
 
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  });
+}
+
 export async function GET(req: NextRequest) {
   // Uzmi naziv videa iz query parametra
   const query = req.nextUrl.searchParams.get("q");
