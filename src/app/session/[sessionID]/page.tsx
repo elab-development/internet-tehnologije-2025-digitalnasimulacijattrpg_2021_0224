40cedefc724845ff19ca40bd46ae39efa2104452
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-03-05 22:00:12 +0100

    poceo sam da radim ui...
    ali socket.io je zlotvor.
    
    dalje elaboriram u sledecem commitu.

--- src/app/session/[sessionID]/page.tsx
+++ src/app/session/[sessionID]/page.tsx
@@ -3,54 +3,42 @@
 import { useEffect, useState } from "react"
 import { socket } from "../../socket"
 import Player from "../../components/Player" 
-
-import { useParams } from 'next/navigation'
-import { user } from "../../types"
+import { campaign } from "../../../../server"
 import { useAuth } from "../../components/AuthProvider"
 import "./session.css"
+
 export default function Session() {
   const {status, user, logout} = useAuth()
-  
-  const {sessionID} = useParams()
 
-  const [players, setPlayers] = useState(Array<user>)
-  const [playerSent, setPlayerSent] = useState(false)
+  const [state, setState] = useState<campaign>()
 
     useEffect(()=> {
-      if (status === 'authenticated' && user && !playerSent) {
-        socket.emit("joinCampaign", sessionID)
-        socket.emit("player", user)
-        setPlayerSent(true)
-        socket.on("pisi", (msg) => {
-          console.log(msg)
-        })
-        socket.on("updatePlayers", (players) => {
-          setPlayers(players)
-          console.log(players)
+      if (status === 'authenticated' && user) {
+        console.log("use effect")
+        socket.emit("updateRequest")
+        console.log("update req emitovan")
+        socket.on("update", (campaign) => {
+          console.log("campaign", campaign)
+          setState(campaign)
+          console.log("state", state)
         })
       }
     })
 
-  console.log(players)
   return (
-  //   <div className="session p-1 border bt-0">
-  //     <div className="players flex flex-col p-1 gap-1">
-  //       {players.map((player)=>(<Player key={player.id} u={player} />))}
-  //     </div>
-  //   </div>
-  // )
-      <div className="layout">
-      <div className="levaStrana">
-        <div className="kampanja bg-blue-500 ">
-          <div className="naslov bg-white-500">sad ce da vidimo nesto</div>
-        </div>
-        <div className="dokument bg-green-500">Dva kostura se dogovarajy djir na motoru</div>
-        <div className="listaIgraca bg-yellow-500" >GAMBATE</div>
-        </div>
-        <div className="sveDesno bg-red-500">
-          <div className="notes bg-pink-500">Zabeleske</div>
-          <div className="karakter bg-purple-500">Statistike o karakteru, kontam da moze da padne jedan CharSheetForm</div>
-        </div>
+    <div className="layout">
+    <div className="levo">
+      <div className="kampanja bg-blue-500 ">
+        <div className="naslov bg-white-500">{state?.name}</div>
+        <div className="opis">{state?.description}</div>
       </div>
+      <div className="dokument bg-green-500">Dva kostura se dogovarajy djir na motoru</div>
+      <div className="listaIgraca bg-yellow-500" >GAMBATE</div>
+      </div>
+      <div className="desno bg-red-500">
+        <div className="notes bg-pink-500">Zabeleske</div>
+        <div className="karakter bg-purple-500">Statistike o karakteru, kontam da moze da padne jedan CharSheetForm</div>
+      </div>
+    </div>
   )
 }
