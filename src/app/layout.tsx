fc1048d6981222deaa7b21eb5d819aca86148ba0
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-03-07 22:16:43 +0100

    devijacija
    ne pitajte..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "./components/navbar";
 import { AuthProvider } from "./components/AuthProvider";
+import { RenderNavbar } from "./components/renderNavbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-        <NavBar />
+        <RenderNavbar />
         {children}
         </AuthProvider>
       </body>
