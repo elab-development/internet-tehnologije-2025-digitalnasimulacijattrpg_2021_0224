80f086f677d77e67deff9a9db1797ce901a5da62
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-03-07 20:03:17 +0100

    Player komponenta sada izgleda drugacije kada je player offlinew

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -8,9 +8,14 @@ interface userProps {
 
 export default function Player({p} : userProps) {
     return (
-        <div className="flex flex-col border p-1 gap-1 bg-black">
+        <div className={`flex flex-col border p-1 gap-1
+            ${ p.online
+                ? "bg-black text-white"
+                : "bg-pink-900 text-pink-950"
+            }`
+        }>
             <p className="border pl-2 pr-2">{p.username}</p>
-            <p className="border pl-2 pr-2">{p.charSheet.name}</p>
+            <p className="border pl-2 pr-2 font-bold">{p.charSheet?.name}</p>
         </div>
     )
 }
